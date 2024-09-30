4d83a78a68e49416c03d991794eaf617350d9d18
Rudolf Kovalov <rudolf.kovalov@gmail.com>
2024-09-30 14:47:10 +0300

    feat(config): update test configs (#10)

--- packages/p-ui/tests/setup.ts
+++ packages/p-ui/tests/setup.ts
@@ -1,6 +1,11 @@
 import { cleanup } from '@testing-library/react';
 import { afterEach } from 'vitest';
+import failOnConsole from 'vitest-fail-on-console';
 import '@testing-library/jest-dom/vitest';
+
+failOnConsole({
+  shouldFailOnDebug: true,
+});
 
 // runs a clean after each test case (e.g. clearing jsdom)
 afterEach(() => {
